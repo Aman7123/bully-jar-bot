4dba88c3f3506e813c820409318ac0311840ea8f
Aaron Renner <arennerwv@gmail.com>
2022-10-19 11:12:50 -0400

    1.1.1 updated logger for when commands register

--- src/main/java/com/aaronrenner/discordnftbot/discord/Commands.java
+++ src/main/java/com/aaronrenner/discordnftbot/discord/Commands.java
@@ -7,6 +7,9 @@ import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.aaronrenner.discordnftbot.discord.commands.*;
 import static com.aaronrenner.discordnftbot.utils.DiscordUtils.userHasAdminPerms;
 
@@ -18,6 +21,7 @@ public class Commands implements MessageCreateListener {
 	
 	// Required
 	private DiscordBot bot;
+	private static final Logger LOGGER  = LoggerFactory.getLogger(Commands.class);
 	
 	public Commands(String commandPrefix, DiscordBot bot) {
 		this.commandPrefix 		= commandPrefix;
@@ -28,32 +32,40 @@ public class Commands implements MessageCreateListener {
 	public void onMessageCreate(MessageCreateEvent event) {
 		/** The message event for this action */
 		Message message = event.getMessage();
+		String parsedMsg = message.getContent().trim();
 		Optional<User> userSender = message.getUserAuthor();
 		Optional<Server> serverSentIn = message.getServer();
 		
-		if (message.getContent().startsWith(commandPrefix)) {
+		if (message.getContent().startsWith(commandPrefix)  && !userSender.get().isYourself()) {
+			User sender = userSender.get();
+			String userName = sender.getDiscriminatedName();
 			
+			LOGGER.info("User {} has triggered the the bot listener with [{}]!", userName, parsedMsg);
 			//
-			if(message.getContent().contains(commandPrefix+"donate")) {
+			if(parsedMsg.contains(commandPrefix+"donate")) {
 				new Donate(this.bot, message);
+				LOGGER.info("User {} has run donate command!", userName);
 			}
 			
 			//
-			if(message.getContent().equalsIgnoreCase(commandPrefix+"jar")) {
+			if(parsedMsg.equalsIgnoreCase(commandPrefix+"jar")) {
 				new Leaderboard(this.bot, message);
+				LOGGER.info("User {} has run jar command!", userName);
 			}
 			
 			// Help message
-			if(message.getContent().equalsIgnoreCase(commandPrefix+"help")) {
+			if(parsedMsg.equalsIgnoreCase(commandPrefix+"help")) {
 				new Help(userSender, serverSentIn, this.botColor, this.bot, message, this.commandPrefix);
+				LOGGER.info("User {} has run help command!", userName);
 			}
 			
 			// Validate admin for all other commands
 			if(userHasAdminPerms(userSender, serverSentIn)) {
 
 				//
-				if(message.getContent().equalsIgnoreCase(commandPrefix+"clear")) {
+				if(parsedMsg.equalsIgnoreCase(commandPrefix+"clear")) {
 					new Clear(this.bot, message);
+					LOGGER.info("User {} has run clear command!", userName);
 				}
 			}
         }
